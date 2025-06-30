8233f54c0f541a53503d99a62321a922c2291b3d
dinhvanhung <boybka23@gmail.com>
2025-06-30 14:51:54 +0700

    Reverted all changes back to version d6e4fe324287a31e36543af165f6c058264184a3

--- src/components/case-management/CaseManagementHeader.tsx
+++ src/components/case-management/CaseManagementHeader.tsx
@@ -84,7 +84,8 @@ export default function CaseManagementHeader({
         </div>
       </div>
 
-    
+      {/* Search and Delete Selected */}
+      <div className="flex items-center justify-between">
         <div className="relative">
           <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 w-4 h-4 text-gray-400" />
           <input

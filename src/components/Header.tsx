cfdf8c1bcd189248a87719ffd020563164a1291e
[dyad] <git@dyad.sh>
2025-06-30 13:51:50 +0700

    [dyad] Thu hẹp chiều cao của header - wrote 1 file(s)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -23,7 +23,7 @@ export default function Header({ onCreateNewBook, onAddUser }: HeaderProps) {
   const [showQuickActions, setShowQuickActions] = useState(false);
 
   return (
-    <header className="bg-white border-b border-gray-200 px-6 py-4">
+    <header className="bg-white border-b border-gray-200 px-6 py-3"> {/* Changed py-4 to py-3 */}
       <div className="flex items-center justify-between">
         {/* Search */}
         <div className="flex-1 max-w-lg">

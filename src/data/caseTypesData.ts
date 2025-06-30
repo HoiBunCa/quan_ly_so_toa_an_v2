d21ca4f20406608b852994d8a9a8c14353fd987d
dinhvanhung <boybka23@gmail.com>
2025-06-30 17:04:04 +0700

    Updated src/data/caseTypesData.ts

--- src/data/caseTypesData.ts
+++ src/data/caseTypesData.ts
@@ -6,7 +6,7 @@ export const caseTypes: CaseType[] = [
     name: 'Sổ thụ lý đơn khởi kiện',
     code: 'CIV',
     attributes: [
-      { id: 'so_thu_ly', name: 'Số thụ lý', type: 'text', required: false, width: 120 },
+      { id: 'so_thu_ly', name: '🔢 Số thụ lý', type: 'text', required: false, width: 120 },
       { id: 'ngay_thu_ly', name: 'Ngày thụ lý', type: 'date', required: false, width: 120 },
       { id: 'thong_tin_nguoi_khoi_kien', name: 'Thông tin người khởi kiện', type: 'textarea', required: false, width: 250 },
       { id: 'thong_tin_nguoi_bi_kien', name: 'Thông tin người bị kiện', type: 'textarea', required: false, width: 250 },
